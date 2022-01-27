b4bfa1158caf2945f85271925e9b0fe03a33c224
siddubhagavan <siddu.bhagavan@gmail.com>
2022-01-27 12:52:44 +0530

    add_delete_modified

---
+++ src/test/java/com/pages/CartPage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is from CartPage");
+	}
+
+}
