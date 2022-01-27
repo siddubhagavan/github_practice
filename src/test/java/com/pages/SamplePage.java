d241392b211e59ba7d2265f475bedbe0be3fca34
siddubhagavan <siddu.bhagavan@gmail.com>
2022-01-27 14:17:23 +0530

    New files are added

---
+++ src/test/java/com/pages/SamplePage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class SamplePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is from sample page");
+	}
+
+}
