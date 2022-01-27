e664a27d56fa3df3f2abea281dd34d7eb3d213d2
siddubhagavan <siddu.bhagavan@gmail.com>
2022-01-27 16:12:58 +0530

    Login Feature Added

---
+++ src/test/java/com/github_practice/LoginScenario.java
@@ -0,0 +1,12 @@
+package com.github_practice;
+
+public class LoginScenario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is from Login Scenario Page");
+		System.out.println("Please login with the valid credentials");
+	}
+
+}
