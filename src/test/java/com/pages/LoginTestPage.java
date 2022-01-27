b67ca349ac261f44fac3aef7ad266c35cde660d0
siddubhagavan <siddu.bhagavan@gmail.com>
2022-01-27 16:21:31 +0530

    login test page added

---
+++ src/test/java/com/pages/LoginTestPage.java
@@ -0,0 +1,12 @@
+package com.pages;
+
+public class LoginTestPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is from login test page");
+
+	}
+
+}
