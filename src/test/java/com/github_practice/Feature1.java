96948eeb7613dee9b46d87d3e7db21e536ceabdb
siddubhagavan <siddu.bhagavan@gmail.com>
2022-01-28 21:35:40 +0530

    Feature1 Added

---
+++ src/test/java/com/github_practice/Feature1.java
@@ -0,0 +1,12 @@
+package com.github_practice;
+
+public class Feature1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is from Feature1");
+
+	}
+
+}
